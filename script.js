f2faaffe7d110c86ca0c8f413c86f930d5cec9dd
Vladimir0657305 <vv0657305@gmail.com>
2022-10-09 20:20:56 +0300

    function that returns both the minimum and maximum number

--- script.js
+++ script.js
@@ -31,3 +31,21 @@ function number (array) {
     }
 }
 number(arrstr);
+
+// function that returns both the minimum and maximum number of the given list/array.
+let arrN = [1, 2, 3, 4, 5];
+function minMax(arr) {
+    let min = arr[0]; 
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min=arr[i]
+        }
+        if (arr[i] > max) {
+            max= arr[i];
+        }
+    }
+    // return [min, max];
+    return(console.log( [min, max])); 
+}
+minMax(arrN);
