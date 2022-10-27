d5c74e8dbb45b2cf69d4f18f0b518d538c721489
Vladimir0657305 <vv0657305@gmail.com>
2022-10-27 22:43:14 +0300

    true if developers from all of the following age groups have signed up

--- script.js
+++ script.js
@@ -740,3 +740,70 @@ for (let j = 0; j < keysSorted.length; j++) {
     }
 }
 
+// true if developers from all of the following age groups have signed up
+let list11 = [
+    { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+    { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+    { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+    { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+    { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+    { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+    { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+function isAgeDiverse(list) {
+    // console.log(list[0].age);
+    let obj = {};
+    for (let i = 0; i < list.length; i++) {
+        console.log(i);
+        if (list[i].age >= 10 && list[i].age <20) {
+            obj.teens = 1;
+        }
+        else if (list[i].age >= 20 && list[i].age < 30) {
+            obj.twenties = 1;
+        }
+        else if (list[i].age >= 30 && list[i].age < 40) {
+            obj.thirties = 1;
+        }
+        else if (list[i].age >= 40 && list[i].age < 50) {
+            obj.fourteens = 1;
+        }
+        else if (list[i].age >= 50 && list[i].age < 60) {
+            obj.fifteens = 1;
+        }
+        else if (list[i].age >= 60 && list[i].age < 70) {
+            obj.sixteens = 1;
+        }
+        else if (list[i].age >= 70 && list[i].age < 80) {
+            obj.seventeens = 1;
+        }
+        else if (list[i].age >= 80 && list[i].age < 90) {
+            obj.eighteens = 1;
+        }
+        else if (list[i].age >= 90 && list[i].age < 100) {
+            obj.nineteens = 1;
+        }
+        else if (list[i].age >= 100) {
+            obj.hundred = 1;
+        }
+    }
+    console.log(Object.keys(obj))
+    if (Object.keys(obj).length < 10) {
+        console.log("false");
+        return false;
+    }
+    else {
+        console.log("true");
+        return true;
+    }
+}
+//   return list.some(h => h.age >= 100) 
+//   ?[10, 20, 30, 40, 50, 60, 70, 80, 90].every(e => list.some(x => x.age - e >= 0 && x.age - e <= 9))
+//   : false;
+// =============================
+// var dec = [/0?\d/, /^1\d$/, /2\d/, /3\d/, /4\d/, /5\d/, /6\d/, /7\d/, /8\d/, /9\d/, /1\d\d/];
+// return dec.every(x => list.some(y => x.test(y.age)));
+// =============================
+// isAgeDiverse(list11);
